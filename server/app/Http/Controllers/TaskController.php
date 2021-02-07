a1c5f5f624ab73333f90b35b438846efa50b2043
takaki <cheap_trick_magic@yahoo.co.jp>
2021-02-07 18:45:50 +0900

    タスク機能の実装(foldersテーブルとの紐付け)

--- server/app/Http/Controllers/TaskController.php
+++ server/app/Http/Controllers/TaskController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        // 全てのフォルダを取得
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐付くタスクを取得
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
         return view('tasks.index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
--- server/database/seeds/FoldersTableSeeder.php
+++ server/database/seeds/FoldersTableSeeder.php
@@ -13,14 +13,29 @@ class FoldersTableSeeder extends Seeder
     public function run()
     {
         DB::table('folders')->delete();
-        $titles = ['プライベート', '仕事', '旅行'];
-
-        foreach ($titles as $title) {
-            DB::table('folders')->insert([
-                'title' => $title,
+        $folder_seeds = [
+            [
+                'id' => 1,
+                'title' => 'プライベート',
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-            ]);
+            ],
+            [
+                'id' => 2,
+                'title' => '仕事',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'id' => 3,
+                'title' => '旅行',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+        foreach ($folder_seeds as $folder) {
+            DB::table('folders')->insert($folder);
         }
     }
 }
+
