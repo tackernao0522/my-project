f54d385bbb4c12eff22aa27e42b259058a554da7
takaki <cheap_trick_magic@yahoo.co.jp>
2021-02-08 15:01:53 +0900

    フォルダ作成機能にバリデーションの設定 エラーメッセージの追加

--- server/resources/views/folders/create.blade.php
+++ server/resources/views/folders/create.blade.php
@@ -12,11 +12,12 @@ Todo
         <nav class="panel panel-default">
           <div class="panel-heading">フォルダを追加する</div>
           <div class="panel-body">
+            @include('error_card_list')
             <form method="post" action="{{ route('folders.create') }}">
               @csrf
               <div class="form-group">
                 <label for="title">フォルダ名</label>
-                <input type="text" class="form-control" name="title" id="title">
+                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
               </div>
               <div class="text-right">
                 <button type="submit" class="btn btn-primary">送信</button>
