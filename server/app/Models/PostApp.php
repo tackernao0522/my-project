e6550b187d2418c934a2825793e8b622e8b53450
takaki <cheap_trick_magic@yahoo.co.jp>
2021-02-23 15:22:04 +0900

    現在のいいね数を算出するメソッドを作成

--- server/app/Models/PostApp.php
+++ server/app/Models/PostApp.php
@@ -22,4 +22,9 @@ class PostApp extends Model
             ? (bool)$this->likes->where('id', $user->id)->count()
             : false;
     }
+
+    public function getCountLikesAttribute(): int
+    {
+        return $this->likes->count();
+    }
 }
