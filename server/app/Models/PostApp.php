2090c6c8f8b969f1394d5dce662b93f4c1d0f1a0
takaki <cheap_trick_magic@yahoo.co.jp>
2021-02-23 14:46:29 +0900

    PostAppモデルにリレーションを追加する

--- server/app/Models/PostApp.php
+++ server/app/Models/PostApp.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class PostApp extends Model
 {
     protected $fillable = [
         'image_file_name', 'title', 'description', 'url', 'user_id'
     ];
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\User', 'likes')->withTimestamps();
+    }
 }
