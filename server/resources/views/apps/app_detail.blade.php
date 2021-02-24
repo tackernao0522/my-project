9bf40521b878eed8e0cd64afee7dc63a9a51e2fa
takaki <cheap_trick_magic@yahoo.co.jp>
2021-02-24 13:19:20 +0900

    投稿アプリケーションの詳細画面の作成-修正2

--- server/resources/views/apps/app_detail.blade.php
+++ server/resources/views/apps/app_detail.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
 
 @section('title')
-  {{ $app->title }} | アプリケーション詳細
+{{ $app->title }} | アプリケーション詳細
 @endsection
 
 @section('content')
 <div class="container-sm">
-    @include('share.flash')
-    @if(Auth::check())
-    <div class="text-center">
-        <span style="height: 100px; font-size: 20px; color: red !important; border-style: ridge; padding: 5px;">{{ $app->user->name }}さんの{{ $app->title }}</span>
-    </div>
+  @include('share.flash')
+  <div class="text-center">
+    <span style="height: 100px; font-size: 20px; color: red !important; border-style: ridge; padding: 5px;">{{ $app->user->name }}さんの{{ $app->title }}</span>
+  </div>
+  <div class="row">
+    @include('share.share_app_details')
+    @if( Auth::id() === $app->user_id )
+    @include('share.share_app_index')
     @endif
-    <div class="row">
-        @include('share.share_app_details')
-        @if( Auth::id() === $app->user_id )
-        @include('share.share_app_index')
-        @endif
-    </div>
+  </div>
 </div>
 </div>
 </div>
